b14998d6120a0dacd9942a424677f54bdc146971
yaqubadesola <yaqub.adesola@gmail.com>
2026-04-13 07:15:03 +0100

    Fix OIDC reference

--- terraform/github-oidc.tf
+++ terraform/github-oidc.tf
@@ -35,7 +35,7 @@ resource "aws_iam_role" "github_actions" {
       {
         Effect = "Allow"
         Principal = {
-          Federated = aws_iam_openid_connect_provider.github.arn
+          Federated = "arn:aws:iam::756418066722:oidc-provider/token.actions.githubusercontent.com"
         }
         Action = "sts:AssumeRoleWithWebIdentity"
         Condition = {
